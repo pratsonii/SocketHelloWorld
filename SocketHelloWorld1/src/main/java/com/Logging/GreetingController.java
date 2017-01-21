1f60273c123ddc729405b275522437f18e4c8930
pratik <pratik@DESKTOP-CK0M5E4>
2017-01-21 15:12:01 +0530

    Project created

---
+++ SocketHelloWorld1/src/main/java/com/Logging/GreetingController.java
@@ -0,0 +1,30 @@
+package com.Logging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+	@Autowired
+	SimpMessagingTemplate template;
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+    
+    @RequestMapping("/test")
+    public @ResponseBody void test()
+    {
+    	template.convertAndSend("/topic/greetings", new Greeting("Hello, !"));
+    }
+
+}
---
+++ SocketHelloWorld1/src/main/java/com/Logging/SocketHelloWorld1Application.java
@@ -0,0 +1,12 @@
+package com.Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketHelloWorld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketHelloWorld1Application.class, args);
+	}
+}
